411c4ec308c75bb45f208769a339cb07a23645ea
Vladimir Anokhin <ano.vladimir@gmail.com>
2023-04-20 11:16:23 +0300

    import wp styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import '@wordpress/block-library/build-style/reset.css'
+import '@wordpress/block-library/build-style/common.css'
+import '@wordpress/block-library/build-style/style.css'
+import '@wordpress/block-library/build-style/theme.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
