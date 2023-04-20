aae78e1285ab62ce667a8abc48624980dd9a6eec
Vladimir Anokhin <ano.vladimir@gmail.com>
2023-04-20 16:51:22 +0300

    remove extra alignments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 import '../styles/global-styles.css'
-import '../styles/alignments.css'
 import '../styles/gutenberg.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
