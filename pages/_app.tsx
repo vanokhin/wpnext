dbf0cfb70257318d717d84ae7755dd3d1c9869d8
Vladimir Anokhin <ano.vladimir@gmail.com>
2023-04-20 14:21:37 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
-import '@wordpress/block-library/build-style/reset.css'
-import '@wordpress/block-library/build-style/common.css'
-import '@wordpress/block-library/build-style/style.css'
-import '@wordpress/block-library/build-style/theme.css'
+import '../styles/gutenberg.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
