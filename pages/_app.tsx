c04dc3c1031c9b648e9383f7e594b503da5bc408
Vladimir Anokhin <ano.vladimir@gmail.com>
2023-04-20 16:25:56 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 import '../styles/global-styles.css'
+import '../styles/alignments.css'
 import '../styles/gutenberg.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
